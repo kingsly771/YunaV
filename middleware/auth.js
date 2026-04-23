0669609aa4c6eeb9f70294a36997d924383311fd
kingsly771 <lightkenshiro@gmail.com>
2026-04-23 10:49:22 +0100

    Delete middleware/auth.js

--- middleware/auth.js
+++
@@ -1,6 +0,0 @@
-module.exports = function requireAuth(req, res, next) {
-  if (!req.session || !req.session.userId) {
-    return res.status(401).json({ error: 'Authentication required' });
-  }
-  next();
-};
